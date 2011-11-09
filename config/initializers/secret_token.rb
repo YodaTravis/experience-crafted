6c0b7b8ea52b05f9e3ff402e06bbc6eab2fab2d5
Travis Williams <yodatravis@gmail.com>
2011-11-08 23:21:33 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExperienceCrafted::Application.config.secret_token = 'e347b47b0079249dd9057552442c0e27c875c95b3351e367e0e2591051d59a0fd48f00eb314f35849d237aee6996f1744ed05528773b75d2b8f991604a9eedb4'
